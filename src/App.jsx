c99cb0ffc62a76d4be566619ea6672434b31048d
Rajkamal202 <raop4903@gmail,com>
2024-07-13 12:02:24 +0530

    Describe the changes you made

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Onboarding1 from './components/Onboarding/Onboarding1'
+
+
+
+function App() {
+ 
+
+  return (
+    <div>  
+    <Onboarding1/> 
+    </div>
+  )
+}
+
+export default App
